6907dab1b0ac7b616d6dc7d95e312b835db28150
CH SaiManoj <chsaimanoj@pogeyan.com>
2018-12-19 18:03:35 +0530

    first commit

---
+++ nonFirstNode/setup.sh
@@ -0,0 +1,192 @@
+#!/bin/bash
+
+#
+# Create all the necessary scripts, keys, configurations etc. to run
+# a cluster of N Quorum nodes with Raft consensus.
+#
+# The nodes will be in Docker containers. List the IP addresses that
+# they will run at below (arbitrary addresses are fine).
+#
+# Run the cluster with "docker-compose up -d"
+#
+# Run a console on Node N with "geth attach qdata_N/dd/geth.ipc"
+# (assumes Geth is installed on the host.)
+#
+# Geth and Constellation logfiles for Node N will be in qdata_N/logs/
+#
+
+# TODO: check file access permissions, especially for keys.
+
+
+#### Configuration options #############################################
+
+read -p "Please enter first node public ip : " first_node_ip
+fn_ip=("$first_node_ip")
+
+read -p "Please enter public IP of this host machine : " node_ip
+ips=("$node_ip")
+
+# Docker image name
+image=quorum
+
+########################################################################
+
+nnodes=${#ips[@]}
+
+./cleanup.sh
+
+uid=`id -u`
+gid=`id -g`
+pwd=`pwd`
+
+#### Create directories for each node's configuration ##################
+
+echo '[1] Configuring for '$nnodes' nodes.'
+
+n=1
+for ip in ${ips[*]}
+do
+    qd=qdata_$n
+    mkdir -p $qd/{logs,keys}
+    mkdir -p $qd/dd/geth
+
+    let n++
+done
+
+
+#### Make static-nodes.json and store keys #############################
+
+echo '[2] Creating Enodes and static-nodes.json.'
+
+echo "[" > static-nodes.json
+n=1
+for ip in ${ips[*]}
+do
+    qd=qdata_$n
+
+    # Generate the node's Enode and key
+    docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/bootnode -genkey /qdata/dd/nodekey -writeaddress
+    key=$(cat $pwd/$qd/dd/nodekey)
+    enode=`docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/bootnode -nodekeyhex $key -writeaddress`
+    # Add the enode to static-nodes.json
+    sep=`[[ $n < $nnodes ]] && echo ","`
+    echo '  "enode://'$enode'@'$ip':30303?discport=0&raftport=50400"'$sep >> static-nodes.json
+
+    # Add the enode to enode-url.json
+    sep=`[[ $n < $nnodes ]] && echo ","`
+    echo '  "enode://'$enode'@'$ip':30303?discport=0&raftport=50400"'$sep >> enode-url.json
+    echo '  [*] Login to geth console of any node of existing cluster & run the following command:'
+    echo '  raft.addPeer("enode://'$enode'@'$ip':30303?discport=0&raftport=50400")'
+
+    let n++
+done
+echo "]" >> static-nodes.json
+
+
+#### Create accounts, keys and genesis.json file #######################
+
+echo '[3] Creating Ether accounts and genesis.json.'
+
+cat > genesis.json <<EOF
+{
+  "alloc": {
+EOF
+
+n=1
+for ip in ${ips[*]}
+do
+    qd=qdata_$n
+
+    # Generate an Ether account for the node
+    touch $qd/passwords.txt
+    account=`docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/geth --datadir=/qdata/dd --password /qdata/passwords.txt account new | cut -c 11-50`
+
+    # Add the account to the genesis block so it has some Ether at start-up
+    sep=`[[ $n < $nnodes ]] && echo ","`
+    cat >> genesis.json <<EOF
+    "${account}": {
+      "balance": "1000000000000000000000000000"
+    }${sep}
+EOF
+
+    let n++
+done
+
+cat >> genesis.json <<EOF
+  },
+  "coinbase": "0x0000000000000000000000000000000000000000",
+  "config": {
+    "homesteadBlock": 0
+  },
+  "difficulty": "0x0",
+  "extraData": "0x",
+  "gasLimit": "0x2FEFD800",
+  "mixhash": "0x00000000000000000000000000000000000000647572616c65787365646c6578",
+  "nonce": "0x0",
+  "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+  "timestamp": "0x00"
+}
+EOF
+
+#### Complete each node's configuration ################################
+
+echo '[4] Creating Quorum keys and finishing configuration.'
+
+n=1
+for ip in ${ips[*]}
+do
+    qd=qdata_$n
+
+    cat tm.conf \
+        | sed s/_NODELIST_/$fn_ip/g \
+        | sed s/_NODEIP_/$ip/g \
+              > $qd/tm.conf
+
+    cp genesis.json $qd/genesis.json
+    cp static-nodes.json $qd/dd/static-nodes.json
+
+    # Generate Quorum-related keys (used by Constellation)
+    docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/constellation-node --generatekeys=qdata/keys/tm < /dev/null > /dev/null
+    echo 'Node '$n' public key: '`cat $qd/keys/tm.pub`
+
+    cat templates/start-node.sh \
+        | sed s/_RAFTID_/$node_number/g \
+              > $qd/start-node.sh
+
+    cp start-node.sh $qd/start-node.sh
+    chmod 755 $qd/start-node.sh
+
+    let n++
+done
+rm -rf genesis.json static-nodes.json
+
+
+#### Create the docker-compose file ####################################
+
+cat > docker-compose.yml <<EOF
+version: '2'
+services:
+EOF
+
+n=1
+for ip in ${ips[*]}
+do
+    qd=qdata_$n
+
+    cat >> docker-compose.yml <<EOF
+  node_$n:
+    image: $image
+    volumes:
+      - './$qd:/qdata'
+    ports:
+      - $((n+22000)):8545
+    expose : [50400, 30303, 9000]
+EOF
+
+    let n++
+done
+
+
+echo '[5] Removing temporary containers.'
+# Remove temporary containers created for keys & enode addresses - Note this will remove ALL stopped containers
+docker container prune -f > /dev/null 2>&1
